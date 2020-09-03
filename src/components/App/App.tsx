3628beeb0f7109e1776b1821a69b8930f8baed2b
Mirudistok32 <mirudistok32@yandex.ru>
2020-09-03 14:44:57 +0300

    css title

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import s from './App.module.scss'
+import { Title } from '../Title/Title';
 
 
 export function App() {
 
-
-  let titleBoxClass = [s.title__box];
-  let displayBoxClass = [s.display__box];
-  let groupButtonsBoxClass = [s['group-buttons__box']];
+  let appClass = [s.app, 'app'].join(' ')
+  let titleBoxClass = [s.title__box].join(' ')
+  let displayBoxClass = [s.display__box].join(' ')
+  let groupButtonsBoxClass = [s['group-buttons__box']].join(' ')
 
 
   return (
-    <div className="app">
-      <div className="app__container">
-        <div className={titleBoxClass.join(' ')}>
-          Click Me Please!
+    <div className={appClass}>
+      <div className="container">
+        <div className={titleBoxClass}>
+          <Title title={'Click Me Please!'} />
           {/* <Title /> */}
         </div>
-        <div className={displayBoxClass.join(' ')}>
+        <div className={displayBoxClass}>
           {/* <Count /> */}
           {/* <Settings /> */}
         </div>
-        <div className={groupButtonsBoxClass.join(' ')}>
+        <div className={groupButtonsBoxClass}>
           {/* <Button /> */}
         </div>
       </div>
--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,16 +1,49 @@
 import React from 'react';
 import s from './Title.module.scss'
 
+type PropsType = {
+  title: string
+}
 
-export const Title = () => {
+export const Title: React.FC<PropsType> = (props) => {
 
-  let titleBoxClass = [s.title__box];
-  let displayBoxClass = [s.display__box];
-  let groupButtonsBoxClass = [s['group-buttons__box']];
+  const { title } = props
 
+  let titleClass = [s.title].join(' ');
+  let spanClass = [s.span].join(' ');
+
+  const collectionStyleColors = [
+    'lightgreen',
+    'lightsalmon',
+    'lightseagreen',
+    'lightcoral',
+    'khaki',
+    'indianred',
+    "hotpink",
+    'gold',
+    'fuchsia',
+    'dodgerblue',
+    'deepskyblue',
+    'darkorange',
+    'crimson'
+  ]
+  // { color: collectionStyleColors[Math.floor(Math.random() * collectionStyleColors.length + 1)] }
+
+  const titleWatching = title.split('').map((i, inx) => {
+    if (i !== ' ') {
+      return <span key={i + inx} className={spanClass}>{i}</span>
+    } else {
+      return i
+    }
+  })
+
+
+  console.log(titleWatching);
   return (
-    <div className="app">
-      
+    <div className={titleClass}>
+      {
+        titleWatching
+      }
     </div>
   );
 }
