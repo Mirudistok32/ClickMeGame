3616d21d2e80c666a4b8ace288b2534d8f9d44ad
Mirudistok32 <mirudistok32@yandex.ru>
2020-09-03 15:04:37 +0300

    Refactor Title

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -7,11 +7,14 @@ type PropsType = {
 
 export const Title: React.FC<PropsType> = (props) => {
 
+  //Пропсы
   const { title } = props
 
+  //Классы
   let titleClass = [s.title].join(' ');
   let spanClass = [s.span].join(' ');
 
+  //коллекция hover цветов
   const collectionStyleColors = [
     'lightgreen',
     'lightsalmon',
@@ -27,16 +30,23 @@ export const Title: React.FC<PropsType> = (props) => {
     'darkorange',
     'crimson'
   ]
-  // { color: collectionStyleColors[Math.floor(Math.random() * collectionStyleColors.length + 1)] }
 
-  const handleMouseEnter = (e: MouseEvent<HTMLSpanElement>) => {
-    e.currentTarget.style.color = collectionStyleColors[Math.floor(Math.random() * collectionStyleColors.length + 1)]
+  //Получение рандомных цветов
+  const randomColor = () => {
+    return collectionStyleColors[Math.floor(Math.random() * collectionStyleColors.length)]
   }
 
+  //Выход указателя мышки из элемента
+  const handleMouseEnter = (e: MouseEvent<HTMLSpanElement>) => {
+    e.currentTarget.style.color = randomColor()
+  }
+
+  //Выход указателя мышки из элемента
   const handleMouseLeave = (e: MouseEvent<HTMLSpanElement>) => {
     e.currentTarget.style.color = '#fff'
   }
 
+  //Строку из пропсов, разделяем по буквенно на массив, а затем возвращаем преобразованный массив. Каждую букву окутываем в span
   const titleWatching = title.split('').map((i, inx) => {
     if (i !== ' ') {
       return <span
