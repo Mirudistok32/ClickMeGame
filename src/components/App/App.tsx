e6d2998c11126a0384172b97ab610d25ecb5f83f
Mirudistok <mirudistok32@yandex.ru>
2020-09-03 12:35:55 +0300

    First

---
+++ src/components/App/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import s from './App.module.scss'
+
+
+
+export function App() {
+
+
+  return (
+    <div className="App">
+      <div className="app__container">
+        
+        <div className={s.title}>
+          Click Me Please!
+        </div>
+        <div className={s.display}>
+
+        </div>
+        <div className={s.button}>
+
+        </div>
+
+      </div>
+    </div>
+  );
+}
