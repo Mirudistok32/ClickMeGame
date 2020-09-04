e9f5cdea43887dfbeb219d102b11f0e121774f43
Mirudistok32 <mirudistok32@yandex.ru>
2020-09-04 10:55:21 +0300

    Updates

---
+++ precache-manifest.c9027efc13e25efca0fbc51c6be0d20e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aeb452058fff2012215636585cf1223",
+    "url": "/ClickMeGame/index.html"
+  },
+  {
+    "revision": "ace88c3ee11973e4d81c",
+    "url": "/ClickMeGame/static/css/main.17b4be5a.chunk.css"
+  },
+  {
+    "revision": "7ab0041d078acbf66c5a",
+    "url": "/ClickMeGame/static/js/2.809ac0f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ClickMeGame/static/js/2.809ac0f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ace88c3ee11973e4d81c",
+    "url": "/ClickMeGame/static/js/main.105345b3.chunk.js"
+  },
+  {
+    "revision": "d4145c4a2d5418849659",
+    "url": "/ClickMeGame/static/js/runtime-main.54e827ad.js"
+  }
+]);
